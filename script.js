4ef8507d653e243bbd9755f3fa264bcc5b55602d
DEBJIT DEBNATH <ddbrosMicrowrks69@outlook.com>
2024-06-28 06:36:21 +0530

    2nd commit

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 // cursor animation.
 let crsr = document.querySelector("#cursor")
 let cr_blur = document.querySelector("#cr_blur")
+let in_h3 = document.querySelectorAll("#nav h3")
+let in_card = document.querySelectorAll("#card_Container .card")
+let in_greenDiv = document.querySelectorAll("#green_div h2")
+let in_quote = document.querySelectorAll("#quotation_div p")
 
 document.addEventListener("mousemove", function(dets){
 
     const posX = dets.clientX;
     const posY = dets.clientY;
-
-    crsr.style.left = `${posX}px`
-    crsr.style.top = `${posY}px`
+   
+    crsr.animate({
+        left: `${posX+2}px`,
+        top: `${posY+10}px`
+    }, {duration: 1000, fill: "forwards" });
     
     cr_blur.animate({
         left: `${posX}px`,
         top: `${posY}px`
     }, {duration: 8000, fill: "forwards" });
-
 })
 
 // nav_bar animation.
@@ -41,5 +46,107 @@ gsap.to("#main", {
         start: "top -25%",
         end: "top -75%",
         scrub: 2
+    }
+})
+gsap.from("#about_Us, #aboutUs_text", {
+    y: 50,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger:{
+        trigger: "#about_Us",
+        scroller: 'body',
+        start: "top 60%",
+        end: "top 55%",
+        scrub: 3,
+        // markers: true
+    }
+})
+gsap.from("#card_Container, .card", {
+    scale: 0.9,
+    duration: 1,
+    opacity: 0,
+    stagger: 0.1,
+    scrollTrigger:{
+        trigger: "#card_Container",
+        scroller: 'body',
+        start: "top 65%",
+        end: "top 60%",
+        scrub: 2,
+        // markers: true
+    }
+})
+gsap.from("#green_div", {
+    y: 80,
+    duration: 2,
+    opacity: 0,
+    scrollTrigger:{
+        trigger: "#green_div",
+        scroller: 'body',
+        start: "top 70%",
+        end: "top 65%",
+        scrub: 3,
+        // markers: true
+    }
+})
+gsap.from("#quotation_div", {
+    scale: 0.9,
+    duration: 1,
+    scrollTrigger:{
+        trigger: "#quotation_div",
+        scroller: 'body',
+        start: "top 55%",
+        end: "top 45%",
+        scrub: 3,
+        // markers: true
+    }
+})
+gsap.from("#colon1", {
+    y: -100,
+    x: -100,
+    duration: 7,
+    scrollTrigger:{
+        trigger: "#colon1",
+        scroller: 'body',
+        start: "top 55%",
+        end: "top 45%",
+        scrub: 4,
+        // markers: true
+    }
+})
+gsap.from("#colon2", {
+    y: 100,
+    x: 100,
+    duration: 7,
+    scrollTrigger:{
+        trigger: "#colon1",
+        scroller: 'body',
+        start: "top 55%",
+        end: "top 45%",
+        scrub: 4,
+        // markers: true
+    }
+})
+gsap.from("#page4 h1", {
+    y: 50,
+    duration: 8,
+    scrollTrigger:{
+        trigger: "#page4 h1",
+        scroller: 'body',
+        start: "top 75%",
+        end: "top 70%",
+        scrub: 3
+        // markers: true
+    }
+})
+gsap.from("#footer h2", {
+    y: 50,
+    duration: 2,
+    scrollTrigger:{
+        trigger: "#footer",
+        scroller: 'body',
+        start: "top 75%",
+        end: "top 70%",
+        scrub: 3
+        // markers: true
     }
 })
